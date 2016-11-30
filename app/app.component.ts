d8745dbf8f3a7227211b0c43d62e9e08f6b6ed2c
chi <chi_bui@outlook.com>
2016-11-30 11:03:59 +1100

    test elements

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,27 @@
 import {Component} from '@angular/core';
 
 @Component({
+    // find the selector tag in index.html
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    // inject into tag the below contents. Better to keep this file skinny. Use a separate file.
+    templateUrl: 'app/app.component.html',
+    styles: [ '.blue-bg{background-color:blue;}' ]
 })
-export class AppComponent { }
+// this class is exported for use everywhere in app
+export class AppComponent {
+//private variable name: datatype is string = value
+  private username:string = "Test"
+  // custom type defined in interface, with object as value
+  private currentUser:User = { username: "Test", email: "test@test.com" }
+
+  private getUsername():string {
+    // get currentUser's username
+    return this.currentUser.username
+    }
+  }
+
+  // interface - similar to classes but only define types. maybe validation here?
+  interface User {
+    username:string,
+    email:string
+  }
