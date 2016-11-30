9c22f307b929472d863047697faff19fe7b783e9
chi <chi_bui@outlook.com>
2016-11-30 12:20:33 +1100

    property binding part 1

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,20 +5,35 @@ import {Component} from '@angular/core';
     selector: 'my-app',
     // inject into tag the below contents. Better to keep this file skinny. Use a separate file.
     templateUrl: 'app/app.component.html',
+    // styling of blue-bg class
     styles: [ '.blue-bg{background-color:blue;}' ]
 })
 // this class is exported for use everywhere in app
 export class AppComponent {
 //private variable name: datatype is string = value
-  private username:string = "Test"
+  // --private username:string = "Test"
   // custom type defined in interface, with object as value
-  private currentUser:User = { username: "Test", email: "test@test.com" }
+  // --private currentUser:User = { username: "Test", email: "test@test.com" }
 
-  private getUsername():string {
+  // --private getUsername():string {
     // get currentUser's username
-    return this.currentUser.username
-    }
+    // --return this.currentUser.username
+    // --}
+  private currentUser:User = { username: "test", email: "test@test.com" }
+
+  // if boolean value not defined, defaults to false
+  private hideEmail:boolean = true
+
+  private disabledInput:boolean = false
+
+  // returns the value of [disabledInput]
+  private isDisabled():boolean {
+    return this.disabledInput
   }
+
+  private isBlueBg:boolean = true
+  private bgColor:string = 'red' // can also be hex or rgba
+}
 
   // interface - similar to classes but only define types. maybe validation here?
   interface User {
